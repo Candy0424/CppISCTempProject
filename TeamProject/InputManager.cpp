7511262134db92324d9fb6a308eb643fce1c7098
san232 <ebs00098@gmail.com>
2025-06-18 17:09:54 +0900

    style : singletonNodeManager

--- TeamProject/InputManager.cpp
+++ TeamProject/InputManager.cpp
@@ -1,17 +1,28 @@
 #include "InputManager.h"
-#include <conio.h>
+#include <Windows.h>
+#include "NodeScroll.h"
 
-void InputManager::Update(NodeManager* nodeManager, bool judgeState[2]) {
-    if (_kbhit()) {
-        char key = _getch();
-        int lane = -1;
-        switch (key) {
-        case 'd': case 'D': lane = 0; break;
-        case 'f': case 'F': lane = 1; break;
-        }
-        if (lane != -1) {
-            judgeState[lane] = true;
-            nodeManager->Judge(lane);
+InputManager* InputManager::instance = nullptr;
+InputManager::InputManager() {}
+
+InputManager* InputManager::GetInstance() {
+    if (!instance) instance = new InputManager();
+    return instance;
+}
+void InputManager::DestroyInstance() {
+    if (instance) { delete instance; instance = nullptr; }
+}
+
+void InputManager::Update(bool judgeState[2]) {
+    static const int VKList[2][2] = { {'D','F'}, {'J','K'} };
+    for (int lane = 0; lane < 2; ++lane) {
+        for (int k = 0; k < 2; ++k) {
+            if (GetAsyncKeyState(VKList[lane][k]) & 0x8000) {
+                judgeState[lane] = true;
+                JudgeResult jr = NodeManager::GetInstance()->Judge(lane);
+                if (jr == JudgeResult::PERFECT || jr == JudgeResult::GOOD)
+                    NodeManager::GetInstance()->RegisterJudgeMsg(lane, jr, 30);
+            }
         }
     }
 }
--- TeamProject/NodeScroll.h
+++ TeamProject/NodeScroll.h
@@ -9,21 +9,34 @@ struct NoteChartInfo {
     int lane;
 };
 
+struct JudgeMsg {
+    JudgeResult result;
+    int frameLeft;
+};
+
 class NodeManager {
 public:
-    NodeManager(int playAreaWidth, int playAreaHeight, int maxNodeCount);
+    static NodeManager* GetInstance(int width = 100, int height = 20, int maxNodeCount = 32);
+    static void DestroyInstance();
     void LoadChart(const std::string& filename);
     void Update(float currentTime);
+    void FillMapBuffer(const bool judgeState[2]);
     void Render(const bool judgeState[2]);
     JudgeResult Judge(int lane);
-    Node* GetNearestJudgeableNode(int lane);
+    Node* GetNearestJudgeableNode(int lane, int judgeRange);
     void HitNode(Node* node);
     int LaneToY(int laneIndex) const;
+    void RegisterJudgeMsg(int lane, JudgeResult res, int duration = 30);
     int judgeLineX;
     int startX;
+    int areaWidth, areaHeight, laneCount;
+    std::vector<std::vector<Tile>> mapBuffer;
+    std::vector<JudgeMsg> judgeMsgs[2];
 private:
+    NodeManager(int playAreaWidth, int playAreaHeight, int maxNodeCount);
+    ~NodeManager();
+    static NodeManager* instance;
     std::vector<Node> nodePool;
     std::vector<NoteChartInfo> chart;
-    int areaWidth, areaHeight, laneCount;
     int nextChartIdx = 0;
 };
