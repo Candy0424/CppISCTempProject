2c08e59810aa45a20b3b63dec136bbba24df5536
san232 <ebs00098@gmail.com>
2025-06-27 22:13:14 +0900

    fix : NodeSkin

--- TeamProject/NodeStyleConfig.h
+++ TeamProject/NodeStyleConfig.h
@@ -7,6 +7,6 @@
 struct NodeStyleConfig {
     NodeSymbolType symbolType = NodeSymbolType::CLASSIC;
     std::vector<std::wstring> classicSymbols{ L"♫", L"♫" };
-    std::vector<std::wstring> alternateSymbols{ L"◆", L"◈" };
+    std::vector<std::wstring> alternateSymbols{ L"█", L"█" };
     COLOR nodeColor = COLOR::YELLOW;
 };
